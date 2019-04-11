7ad060219620734918b45da6d648bbc46b060da6
sally <sally.matson@gmail.com>
2019-04-11 18:15:33 -0400

    update parsing alg

--- utils.py
+++ utils.py
@@ -1,68 +1,55 @@
-def parse_old_corpus_tweets(filename='RAW_CORPUS.txt'):
+import json
+import re
+
+START = "<s> "
+END = " </s>"
+MEDIA = "<MEDIA>"
+
+
+def parse_twitter_api_tweets(filename, tweets=[], ids=[]):
     '''
-    Parses tweets specifically from "RAW_CORPUS.txt", a corpus of Donald Trump's tweets 
-    between __ and ___. 
-    
-    Textfile format is tab separated with headings:  
-    STATUSID    DATE    TIME    SOURCE  Retweet Favourite   TWEETS
+    Parse output from scrape_tweets in twitter_api.py
+    Adds some tweet formatting 
     '''
-    tweets = []
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='UTF-8') as f:
         header = f.readline()
         line = f.readline()
+
         while line:
             if line != "\n":
                 line = line.strip()
-                tweet_id, date, time, _, _, _, text = line.split("\t")
-                tweet_id = tweet_id[1:].strip()
-                text = text.replace('\xa0', ' ')
-                tweets.append([tweet_id, date, time, text])
+                # Depending on the corpus format, split differently 
+                if len(line.split("\t")) == 7:
+                    tweet_id, date, time, _, _, _, text = line.split("\t")
+                    tweet_id = tweet_id[1:].strip()
+                else:
+                    tweet_id, date, time, text = line.split("\t")
 
-            line = f.readline()
-    return tweets
+                if tweet_id not in ids: # filter out overlapping tweets
+                    ids.append(tweet_id)
+                    # replace a few special chars that show up weird in the file:
+                    text = text.replace('\xa0', ' ')
+                    text = text.replace('&amp;', '&')
+                    text = text.replace('\u2019', r"'")
+                    text = text.replace('\u201c', r'"')
+                    text = text.replace('\u201d', r'"')
+                    # Replace any URL with a tag:
+                    text = re.sub('https://[^\s]+', MEDIA, text)
+                    # add whitespace before & after punctuation:
+                    text = re.sub("[^A-Za-z0-9\s#@<>']", ' \g<0> ', text);
+                    text = START + text
+                    tweet = text + END
+                    tweets.append([tweet_id, date, time, tweet])
 
-def parse_twitter_api_tweets(filename):
-    '''
-    Parses tweets in the format that we use in our twitter scraping
-    (see twitter_api.py -> scrape_tweets())
-    '''
-    tweets = []
-    with open(filename, 'r', encoding='utf-8') as f:
-        header = f.readline()
-        line = f.readline()
-        while line:
-            if line != "\n":
-                line = line.strip()
-                tweet_id, date, time, text = line.split("\t")
-                text = text.replace('\xa0', ' ')
-                tweets.append([tweet_id, date, time, text])
+                    # next line
+                    line = f.readline()
 
-            line = f.readline()
-    return tweets
+                else: # We have already seen this tweet! 
+                    line = f.readline()
 
-def get_donald_trump_tweets():
-    all_tweets = []
-    old_tweets = parse_old_corpus_tweets()
-    new_tweets = parse_twitter_api_tweets('realdonaldtrump_raw_tweets.txt')
-    # todo: get unique 
-    #for tweet_list in old_tweets: 
-    #    all_tweets.append(tweet_list[3])
-    for tweet_list in new_tweets: 
-        all_tweets.append(tweet_list[3])
-    all_tweets = clean_tweets(all_tweets)
-    return all_tweets
+    return tweets, ids
 
-def clean_tweets(tweets):
-    '''
-    -> Replaces any link (signifies quote tweet or photo) with <MEDIA/> tag.
-    -> Adds start and end tags for tweets. 
-    '''
-    import re
-    cleaned_tweets = []
-    for tweet in tweets:
-        tweet = re.sub('https://[^\s]+', '<MEDIA/>', tweet)
-        tweet = "<s> " + tweet + " </s>"
-        cleaned_tweets.append(tweet)
+tweets, ids = parse_twitter_api_tweets("AOC_raw_tweets.txt")
 
-    return cleaned_tweets
-
+with open('ALL_AOC_TWEETS.txt', 'w', encoding='utf-8') as outfile:  
+    json.dump(tweets, outfile)
