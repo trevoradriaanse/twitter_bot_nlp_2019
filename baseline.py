bd068ce2e7290679444dcb6562bb34c44379aecd
sally <sally.matson@gmail.com>
2019-04-21 15:24:06 -0400

    updating baseline to have same parsing as NN

--- baseline.py
+++ baseline.py
@@ -1,6 +1,9 @@
+import argparse
+import sys
 from nltk import word_tokenize
 import numpy as np
 import codecs
+import json
 from generate import GENERATE
 from sklearn.preprocessing import normalize
 
@@ -12,8 +15,7 @@ from sklearn.preprocessing import normalize
 def return_set(tweets):
     result = []
     for tweet in tweets:
-        tokens = word_tokenize(tweet)
-        words = [word.lower() for word in tokens if word.isalpha()]
+        words = tweet.split(" ")
         result.extend(words)
     return list(set(result))
 
@@ -30,22 +32,26 @@ def bigram(tweets, tweet_dict):
     counts += 0.1
     previous_word = '<s>'
     for tweet in tweets:
-        tokens = word_tokenize(tweet)
+        tokens = tweet.split(" ")
         for token in tokens:
-            if token.isalpha():
-                counts[tweet_dict[previous_word.lower()]][tweet_dict[token.lower()]] += 1
-                previous_word = token
+            counts[tweet_dict[previous_word]][tweet_dict[token]] += 1
+            previous_word = token
     probs = normalize(counts, norm='l1', axis=1)
     probs = probs.transpose()
     # print(probs)
     return probs
 
+parser = argparse.ArgumentParser(description='Run the Baseline Model')
+parser.add_argument('-tf','--tweets-filepath', default="", type=str, help='File path to load tweets')
 
 if __name__ == '__main__':
-    tweets = codecs.open("ALL_AOC_TWEETS.txt")
+    args = parser.parse_args()
+    with open(args.tweets_filepath) as o:
+        tweets_json = json.load(o)
+    tweets = [tweet[3] for tweet in tweets_json]
     tweet_word = return_set(tweets)
     tweet_dict = make_dict(tweet_word)
     probs = bigram(tweets, tweet_dict)
     # Generate
     for i in range(0, 9):
-        print(GENERATE(tweet_dict, normalize(probs, norm='l1', axis=1), 'bigram', 8, "the") + "\n")
+        print(GENERATE(tweet_dict, normalize(probs, norm='l1', axis=1), 'bigram', 20, "<s>") + "\n")
