14dc08313fa3fa5ccfb98471977f04ff3ebf4251
FrankOnBeach <frank25331847@gmail.com>
2019-05-01 02:36:25 -0400

    Updated Script file

---
+++ generate_text_script_small.sh
@@ -0,0 +1,5 @@
+python models.py -tf TRUMP_500_tweets.txt -t "load" -g "regular" -e 50 -do 0.3 -ea -1 -em 50 -hs 50 -wf "lstm_500_trump_1.hdf5"
+python models.py -tf TRUMP_500_tweets.txt -t "load" -g "regular" -e 50 -do 0.3 -ea -1 -em 100 -hs 100 -wf "lstm_500_trump_2.hdf5"
+python models.py -tf TRUMP_500_tweets.txt -t "load" -g "regular" -e 50 -do 0.3 -ea -1 -em 150 -hs 150 -wf "lstm_500_trump_3.hdf5"
+python models.py -tf TRUMP_500_tweets.txt -t "load" -g "regular" -e 50 -do 0.3 -ea -1 -em 200 -hs 200 -wf "lstm_500_trump_4.hdf5"
+
